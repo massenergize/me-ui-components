f094541bc2d7fa2965eec6350c1f53b9eb4ba1a4
frimpongopoku <pongofrimi@Gmail.com>
2022-04-19 12:11:15 +0400

    font icon dialog complete :fire: :ok_hand:

---
+++ src/stories/IconDialog.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import IconDialog from "../icon dialog/IconDialog";
+
+export default {
+  title: "Components/ Icon Dialog",
+  Component: IconDialog,
+};
+
+const Template = (props) => <IconDialog {...props} />;
+
+export const Default = Template.bind({});
